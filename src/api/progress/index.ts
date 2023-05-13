2e5c79e878bc799a3f4c02e9f3db4f9db6994f77
Erik <erik.ondra@yahoo.com>
2023-05-13 23:42:54 +0200

    deleting tracks and charts function

--- src/api/progress/index.ts
+++ src/api/progress/index.ts
@@ -26,4 +26,49 @@ progressRouter.get(
   }
 );
 
+progressRouter.delete(
+  "/track/:trackId",
+  JWTAuthMiddleware,
+  async (req: UserRequest, res, next) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ error: "Unauthorized" });
+      } else {
+        await ProgressModel.findByIdAndDelete({
+          _id: req.params.trackId,
+        });
+        return res.status(202).json({ deleted: "success" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+progressRouter.delete(
+  "/chart/:exerciseId",
+  JWTAuthMiddleware,
+  async (req: UserRequest, res, next) => {
+    try {
+      if (!req.user) {
+        return res.status(401).json({ error: "Unauthorized" });
+      } else {
+        console.log(
+          "exercise_id:",
+          req.params.exerciseId,
+          "user_id:",
+          req.user._id
+        );
+        await ProgressModel.deleteMany({
+          user_id: req.user._id,
+          exercise_id: req.params.exerciseId,
+        });
+        return res.status(202).json({ deleted: "success" });
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 export default progressRouter;
